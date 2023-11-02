14e371f33e96e499dea85fe7ae009fdd7ae2095b
Richard Khan <rkhan@khanrichard.com>
2023-11-02 01:03:24 -0400

    setup up angualr app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './screens/projects/projects.component';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './common/navbar/navbar.component';
+
+const routes: Routes = [
+  // Existing routes...
+  { path: '', component: ProjectsComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/screens/projects/projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent {
+  title = 'Projects';
+}
